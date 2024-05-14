e47e54c2c0f0b4effbd47c64768f602097804dff
tbjuechen <1324374092@qq.com>
2024-05-14 16:03:52 +0800

    分类器

--- test.py
+++ test.py
@@ -5,10 +5,22 @@ from fastapi import FastAPI,Response
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
+import cv2 
+
+# 加载Haar级联分类器
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+
 app = FastAPI()
 
-PHOTOPATH = os.path.join(os.path.expanduser('~'),'Downloads','集美')
+PHOTOPATH = os.path.join(os.path.expanduser('~'),'Downloads','beauty')
 
+def is_hear_photo(url: str)->bool:
+    img = cv2.imread(url)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    print(len(faces))
+    return len(faces)>0
 
 def choose_random_file(directory):
     file_list = os.listdir(directory)
@@ -18,7 +30,12 @@ def choose_random_file(directory):
 
 @app.get('/photo')
 def push_photo():
-    random_file = choose_random_file(PHOTOPATH)
+    while (True):
+        random_file = choose_random_file(PHOTOPATH)
+        file_path = os.path.join(PHOTOPATH,random_file)
+        if is_hear_photo(file_path):
+            break
+
     with open(os.path.join(PHOTOPATH,random_file), 'rb') as f:
         photo = f.read()
     return Response(content=photo, media_type="image/jpeg")
